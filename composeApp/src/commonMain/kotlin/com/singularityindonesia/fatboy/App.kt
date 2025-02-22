2245079e9e39aaa5cce21137346954d7d7432472
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-02-22 20:15:57 +0700

    main: simplify

--- composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/App.kt
+++ composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/App.kt
@@ -1,9 +1,14 @@
 package com.singularityindonesia.fatboy
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.singularityindonesia.core.navigation.DestinationProvider
+import com.singularityindonesia.fatboy.ui.navigation.HomeBottomNavigator
+import com.singularityindonesia.fatboy.ui.navigation.MainPlot
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -13,9 +18,23 @@ fun App() {
 
     MaterialTheme {
         DestinationProvider(navController) {
-            Main(
-                navController = navController
-            )
+            Scaffold(
+                topBar = {
+
+                },
+                bottomBar = {
+                    HomeBottomNavigator(
+                        onItemClicked = {
+                            navController.navigate(route = it.route)
+                        }
+                    )
+                }
+            ) { padding ->
+                MainPlot(
+                    modifier = Modifier.padding(padding),
+                    navController = navController
+                )
+            }
         }
     }
 }
