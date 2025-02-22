8b88463274e5c7a3ac4aeb7bfbc3a7cee1a718e9
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-02-22 20:19:04 +0700

    main: extract provider to composable function

--- composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/App.kt
+++ composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/App.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.singularityindonesia.core.navigation.DestinationProvider
 import com.singularityindonesia.fatboy.ui.navigation.HomeBottomNavigator
@@ -17,7 +18,7 @@ fun App() {
     val navController = rememberNavController()
 
     MaterialTheme {
-        DestinationProvider(navController) {
+        Provider(navController) {
             Scaffold(
                 topBar = {
 
@@ -36,5 +37,18 @@ fun App() {
                 )
             }
         }
+    }
+}
+
+/**
+ * Provides everything here
+ */
+@Composable
+private fun Provider(
+    navController: NavHostController,
+    content: @Composable () -> Unit
+) {
+    DestinationProvider(navController) {
+        content()
     }
 }
