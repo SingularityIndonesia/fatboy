86311e51be152295e47e28c6c6ba366b7b9d5764
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-02-23 12:41:19 +0700

    dashboard: apply styling in bottom navigator

--- composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/ui/navigation/HomeBottomNavigator.kt
+++ composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/ui/navigation/HomeBottomNavigator.kt
@@ -1,9 +1,14 @@
 package com.singularityindonesia.fatboy.ui.navigation
 
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.singularityindonesia.core.navigation.CurrentDestination
 import com.singularityindonesia.fatboy.ui.route.HomeBottomNavigationRoute
 import com.singularityindonesia.fatboy.ui.route.Route
@@ -14,7 +19,12 @@ fun HomeBottomNavigator(
 ) {
     val menus = remember { HomeBottomNavigationRoute.menus }
 
-    BottomAppBar {
+    BottomAppBar(
+        modifier = Modifier.clip(
+            RoundedCornerShape(16.dp,16.dp,0.dp,0.dp)
+        ),
+        containerColor = Color.White,
+    ) {
         menus.map {
             NavigationBarItem(
                 selected = CurrentDestination.current == it.route,
@@ -27,6 +37,12 @@ fun HomeBottomNavigator(
                 label = {
                     it.Label()
                 },
+                colors = NavigationBarItemDefaults.colors().copy(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    // fixme: use system color
+                    selectedIndicatorColor = Color.Transparent
+                )
             )
         }
     }
