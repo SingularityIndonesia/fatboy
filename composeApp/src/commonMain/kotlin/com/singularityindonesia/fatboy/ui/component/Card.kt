648cc22283122d188fb8327055f73d321c87b855
Muhammad-Jafar <131.powerfull@gmail.com>
2025-03-02 09:45:40 +0800

    feat(profile): add profile screen ui variant 1

--- composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/ui/component/Card.kt
+++ composeApp/src/commonMain/kotlin/com/singularityindonesia/fatboy/ui/component/Card.kt
@@ -1,15 +1,18 @@
 package com.singularityindonesia.fatboy.ui.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,5 +41,31 @@ fun MediumCard(
             contentAlignment = contentAlignment,
             content = content,
         )
+    }
+}
+
+@Composable
+fun CardWrapper(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.surfaceContainerLow,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .fillMaxWidth()
+            .then(modifier),
+        colors = CardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            disabledContentColor = MaterialTheme.colorScheme.outline,
+            disabledContainerColor = MaterialTheme.colorScheme.onSurface
+        ),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        content()
     }
 }
